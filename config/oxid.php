edc8448d434eed46e71dc872ab033ee27c15af93
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2018-02-13 23:28:02 +0100

    add libraries twig/twig, league/flysystem and configure the di

--- config/oxid.php
+++ config/oxid.php
@@ -6,11 +6,37 @@
  * file that was distributed with this source code.
  */
 
-use function DI\object, DI\get;
+use function DI\object, DI\get, DI\value;
 use OxidEsales\Facts\Facts;
 use OxidEsales\UnifiedNameSpaceGenerator\UnifiedNameSpaceClassMapProvider;
+use League\Flysystem;
+use Psr\Container\ContainerInterface;
 
 return [
+    'path' => value(dirname(__DIR__)),
     Facts::class => object(),
     UnifiedNameSpaceClassMapProvider::class => object()->constructor(get(Facts::class)),
+
+    // twig
+    'twig.options' => [],
+    Twig_LoaderInterface::class => object(Twig_Loader_Filesystem::class)->constructor(get('path')),
+    Twig_Environment::class => object()->constructor(
+        get(Twig_LoaderInterface::class),
+        get('twig.options')
+    ),
+
+    // filesystem
+    'createFs' => value(function(Flysystem\AdapterInterface $adapter) {
+        return (new Flysystem\Filesystem($adapter))
+            ->addPlugin(new Flysystem\Plugin\ListPaths)
+            ->addPlugin(new Flysystem\Plugin\ListFiles());
+    }),
+    Flysystem\Adapter\Local::class => object()->constructor(get('path')),
+    Flysystem\Filesystem::class => function(Flysystem\Adapter\Local $local, ContainerInterface $c) {
+        $createFs = $c->get('createFs');
+        return $createFs($local);
+    },
+    Flysystem\MountManager::class => function(Flysystem\Filesystem $fs) {
+        return new Flysystem\MountManager(['fs' => $fs]);
+    }
 ];
