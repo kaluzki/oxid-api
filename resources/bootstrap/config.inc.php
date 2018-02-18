73f0f5444c1bcce6d47ffab1251a3b72de6578be
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2018-02-18 20:42:38 +0100

    suppress loading config value `aModules` from dB
    see Core\Module\ModuleChainsGenerator::getModulesArray

--- resources/bootstrap/config.inc.php
+++ resources/bootstrap/config.inc.php
@@ -5,3 +5,14 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+\fn\traverse([
+    'sShopDir' => OX_BASE_PATH,
+    'sCompileDir' => OX_BASE_PATH . 'tmp/',
+    'blProductive' => false,
+    'aLanguageURLs' => [],
+], function($value, $name) {
+    $this->$name = $value;
+    // configuration parameters in $this->_aConfigParams have the highest priority
+    $this->_aConfigParams[$name] = $value;
+});
